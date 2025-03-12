7cf571da52982fb915a28c46a511aa701ce63800
TechEvgeny <v.c_kokoc@mail.ru>
2025-03-12 22:30:13 +0300

    PP2_1_3V1

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,10 @@
+package app.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component ("dogBean")
+public class Dog extends Animal {
+
+}
